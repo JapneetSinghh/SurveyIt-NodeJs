abb7871c2a38bf22020b6d3fb2c32e3732414e24
Japneet Singh <japneet8208@gmail.com>
2022-11-13 21:10:36 +0530

    Saved  first survey  response to database

--- Controllers/responses.js
+++ Controllers/responses.js
@@ -1,7 +1,110 @@
-const survey = require('../Models/survey');
 const Survey = require('../Models/survey');
+const Response = require('../Models/responses');
+
 exports.postNewResponse = (req, res, next) => {
-    console.log(req.body);
+    // FETCHING DATA FROM BODY
+    const surveyId = req.body.surveyId;
+    const questionsData = [];
+    const numOfQues = req.body.questionNumber.length;
+    const username = req.body.userName;
+    const email = req.body.userEmail;
+    // FETCHING QUESTIONS , ANSWERS, QUESTION TYPE ETC 
+    for (var i = 1; i < numOfQues; i++) {
+        var questionNumber = i;
+        var questionType = req.body[`question_${i}_type`];
+        var question = req.body[`question_${i}`];
+        var questionDescription = req.body[`question_${i}_description`];
+        var isRequired = req.body[`question_${i}_required`];
+        var answer = req.body[`question_${i}_answer`]
+        if (!questionDescription) {
+            questionDescription = '';
+        }
+
+        if (questionType === 'MCQ') {
+            var correctOption = '';
+            var numOfOptions = req.body[`question_${i}_number_of_options`];
+            var options = [];
+            var serialCharNum = 65;
+            for (var j = 1; j <= numOfOptions; j++) {
+                var serial = String.fromCharCode(serialCharNum);
+                serialCharNum++;
+                options.push(
+                    {
+                        serial: serial,
+                        option: req.body[`question_${i}_option_${j}`]
+                    }
+                )
+            }
+            // ADDING QUESTION DATA TO AN OBJECT OBJ, 
+            // IF QUESTION TYPE IS MCQ ADDING OPTIONS TO OBJ
+            var obj = {
+                questionNumber: questionNumber,
+                type: questionType,
+                question: question,
+                isRequired: isRequired,
+                correctOption: correctOption,
+                numberOfOptions: numOfOptions,
+                options: options,
+                answer: answer
+            }
+        } else {
+            var obj = {
+                questionNumber: questionNumber,
+                type: questionType,
+                question: question,
+                questionDescription: questionDescription,
+                isRequired: isRequired,
+                answer: answer
+            }
+        }
+        questionsData.push(obj);
+    }
+
+    // SETTING THE CORRECT OPTIONS FROM SURVEY
+    // NOT CORRECT OPTIONS WERE NOT INCLUDED IN BODY AS WE DONT WANT OUR USERS TO VIEW THEM 
+
+    function setOptions(questionSurvey, i) {
+        questionsData.forEach((questionResponse, index) => {
+            if (questionResponse.type === 'MCQ' && index === i) {
+                questionResponse.correctOption = questionSurvey.correctOption;
+            }
+        })
+    }
+    // PROMISE FOR SETTING OPTIONS
+    const myPromise = new Promise((resolve, reject) => {
+        Survey.findById(surveyId)
+            .then(data => {
+                data.questionsData.forEach((questionSurvey, index) => {
+                    if (questionSurvey.type === 'MCQ') {
+                        setOptions(questionSurvey, index);
+                    }
+                })
+                setTimeout(() => {
+                    resolve();
+                    reject();
+                }, 2000)
+            })
+    })
+    myPromise.then(resp => {
+        // Saving the response
+        const response = new Response({
+            username: username,
+            email: email,
+            questionsData: questionsData,
+            surveyId: surveyId
+        });
+        response.save().then(result => {
+            console.log(result);
+            res.redirect('/');
+        })
+            .catch(err => {
+                console.log(err);
+            })
+    })
+        .catch(err => {
+            console.log(err);
+        })
+
 }
 exports.getSurvey = (req, res, next) => {
     const surveyId = req.params.surveyId;
@@ -34,3 +137,4 @@ exports.getFullResponse = (req, res, next) => {
         path: "/responses"
     });
 }
+
